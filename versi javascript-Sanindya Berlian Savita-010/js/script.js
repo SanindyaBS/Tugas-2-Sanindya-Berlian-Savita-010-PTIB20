fe21c3dbef80d76bc431e8dc1da68f59f173a93a
SanindyaBS <82386002+SanindyaBS@users.noreply.github.com>
2022-03-14 14:40:37 +0700

    Initial commit

---
+++ versi javascript-Sanindya Berlian Savita-010/js/script.js
@@ -0,0 +1,10 @@
+/* Memunculkan Pop Up Button */
+alert("Selamat Datang Di Website Pintar");
+
+/* Memanipulasi Teks */
+document.getElementById("bunga1").innerHTML = "helianthus paradoxus";
+
+document.getElementById("bunga2").innerHTML = "Helianthus debilis";
+
+document.getElementById("bunga3").innerHTML = "Helianthus laetiflorus";
+
